5f6eb48562d2496b69e121fd8b53f3f948cac33a
Sam Meech-Ward <sam@meech-ward.me>
2022-01-05 11:50:02 -0800

    use port env var

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ app.post("/api/foods", (req, res) => {
   res.send(req.body)
 })
 
-app.listen(8080, () => console.log("listening on port 8080"))
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log("listening on port 8080"))
